0dcfd17dc332e662a6e2008a3a7ef5654d38e333
Kevin Yobeth <leokeviny@gmail.com>
2020-08-14 19:31:57 +0700

    Added landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    {{-- Plz --}}
+    <img class="landIllustration" src="img/storageIllustration.svg" alt="" height="360">
+    <div class="landText">
+        <h1 style="font-weight: bold; font-size: 80px">NEW FACE</h1>
+        <h2>FOR A LOCALHOST STORAGE</h2>
+    </div>
+
+</div>
+@endsection
