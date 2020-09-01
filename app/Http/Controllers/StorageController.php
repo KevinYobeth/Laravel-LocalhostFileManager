da980ee80b0c8178f68b3bafe55abb28ae5468e1
Kevin Yobeth <leokeviny@gmail.com>
2020-09-01 18:54:58 +0700

    Added frontend for storage

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -14,17 +14,21 @@ class StorageController extends Controller
 
         $basenameArray = array();
         $extensionArray = array();
+        $sizeArray = array();
 
         $filesInFolder = \File::files('../../video');
         foreach ($filesInFolder as $path) {
             $file = pathinfo($path);
+            $fileSize = \File::size(public_path($path));
+
+            $sizeArray = Arr::prepend($sizeArray, $fileSize);
             $basenameArray = Arr::prepend($basenameArray, $file['basename']);
             $extensionArray = Arr::prepend($extensionArray, $file['extension']);
             // error_log($file['basename']);
             // error_log($file['extension']);
         }
-
         return view('storage', [
+            'size' => $sizeArray,
             'basename' => $basenameArray,
             'extension' => $extensionArray
         ]);
--- resources/views/storage.blade.php
+++ resources/views/storage.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
 <main class="py-4 solidbg">
-    <div class="flex-center position-ref full-height">
-        @foreach(range(0, count($basename) - 1) as $y)
-            <ul class="itemText">
-                <a href="./storage/{{ $y }}">
-                    <li>{{ $basename[$y] }}</li>
-                </a>
-                {{-- <li>{{$extension[$y] }}</li> --}}
-            </ul>
-        @endforeach
+    <div class="container">
+        <div class="row separator d-flex justify-content-center">
+            @foreach(range(0, count($basename) - 1) as $y)
+
+                <div class="card col-md-5" style="width: 18rem; margin: 5px">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $basename[$y] }}</h5>
+                        <p class="card-text">
+                            Extension: .{{ $extension[$y] }} <br>
+                            Size: {{ round($size[$y] / 1024 / 1024, 2) }} MB</p>
+                        <a href="./storage/{{ $y }}" class="btn btn-primary">View File</a>
+                    </div>
+                </div>
+
+            @endforeach
+        </div>
     </div>
+
 </main>
 @endsection
